aebb9a30dbff67fbef4e253b2d54fc20674eb17f
kossLAN <kosslan@kosslan.dev>
2026-01-07 03:54:02 -0500

    undo carapace changes

--- nix/config.nix
+++ nix/config.nix
@@ -14,51 +14,34 @@ in {
     gstat # git info
   ];
 
-  extraConfig = /*nu*/
-  ''
-    # integrate carapace
-    source ${
-      pkgs.runCommand "carapace-nushell-config.nu" {} ''
-        ${carapace} _carapace nushell | sed 's|"/homeless-shelter|$"($env.HOME)|g' >> "$out"
-      ''
-    }
+  extraConfig =
+    # nu
+    ''
+      # integrate carapace
+      source ${
+        pkgs.runCommand "carapace-nushell-config.nu" {} ''
+          ${carapace} _carapace nushell | sed 's|"/homeless-shelter|$"($env.HOME)|g' >> "$out"
+        ''
+      }
 
-    let carapace_completer = {|spans|
-      let cmd_tokens = ($spans | each {|s| $s })
+      let carapace_completer = {|spans|
+          ${carapace} $spans.0 nushell ...$spans | from json
+      }
 
-      let carapace_json = (
-        (${carapace} $cmd_tokens.0 nushell ...$cmd_tokens) | from json
-      )
-
-      let hist_values = (
-        history
-        | get command
-        | where $it != ""
-        | first 50
-      )
-
-      let hist_completions = (
-        $hist_values
-        | each {|c| { value: $c, description: "recent" } }
-      )
-
-      { completions: ($hist_completions + $carapace_json) }
-    }
-
-    $env.config = ($env.config | default {} | merge {
-        completions: {
-            case_sensitive: false # case-sensitive completions
-            quick: true    # set to false to prevent auto-selecting completions
-            partial: true    # set to false to prevent partial filling of the prompt
-            algorithm: "fuzzy"    # prefix or fuzzy
-            external: {
-                # set to false to prevent nushell looking into $env.PATH to find more suggestions
-                enable: true
-                # set to lower can improve completion performance at the cost of omitting some options
-                max_results: 100
-                completer: $carapace_completer # check 'carapace_completer'
-            }
-        }
-    })
-  '';
+      $env.config = ($env.config | default {} | merge {
+          completions: {
+              case_sensitive: false # case-sensitive completions
+              quick: true    # set to false to prevent auto-selecting completions
+              partial: true    # set to false to prevent partial filling of the prompt
+              algorithm: "fuzzy"    # prefix or fuzzy
+              external: {
+                  # set to false to prevent nushell looking into $env.PATH to find more suggestions
+                  enable: true
+                  # set to lower can improve completion performance at the cost of omitting some options
+                  max_results: 100
+                  completer: $carapace_completer # check 'carapace_completer'
+              }
+          }
+      })
+    '';
 }
